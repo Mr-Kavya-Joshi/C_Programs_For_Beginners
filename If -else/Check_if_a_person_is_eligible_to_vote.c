90b04e0fb2c201d3ba8499014f94c18655e3d746
Kavya Joshi <emailkavya03@gmail.com>
2026-04-08 12:20:29 +0530

    Create Check_if_a_person_is_eligible_to_vote.c

---
+++ If -else/Check_if_a_person_is_eligible_to_vote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int age;
+    printf("Enter your age in years:\n");
+    scanf("%d",&age);
+        
+      if(age<18) printf("You are not eligible to vote!");
+      else printf("You are eligible to vote!");
+    
+    return 0;
+}
