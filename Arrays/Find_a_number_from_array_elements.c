ff7accd2da393b805b1a585f763008741aa744d4
Kavya Joshi <emailkavya03@gmail.com>
2026-04-21 13:05:14 +0530

    Create Find_a_number_from_array_elements.c

---
+++ Arrays/Find_a_number_from_array_elements.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+  int find;
+  int num;
+  printf("Enter the number you want to find : ");
+  scanf("%d",&num);
+  int i;  
+  int arr[]={2,2,2,7,4,6,9,34};
+  for(i=0;i<6;i++){
+    if(arr[i]==num) 
+      printf("Yes, it is present in the array!");
+    else 
+      printf("No, it is not present in the array.");
+    break;
+   }
+  return 0;
+}
