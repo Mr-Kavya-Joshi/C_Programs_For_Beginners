4227d815942af4fc16bb68d46aa9d0d9687e81e0
Kavya Joshi <emailkavya03@gmail.com>
2026-04-23 16:24:25 +0530

    Create Series_1+2+3+...+N.c

---
+++ While and do while loops/Series_1+2+3+...+N.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int N;
+  int sum=0;
+  printf("enter the value of N: ");
+  scanf("%d",&N);
+  for(int i=1;i<=N;i++){
+    sum+=i;
+  }
+  printf("The sum is: %d",sum);
+  return 0;
+}
